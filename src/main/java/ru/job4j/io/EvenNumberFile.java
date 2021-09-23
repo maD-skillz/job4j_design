ce497b35656286cc8c4d7ce7f8046cdf96b40b8e
maD-skillz <warneverchang3s@gmail.com>
2021-09-23 11:32:07 +0300

    0.2. FileInputStream [#4898 #206961]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -5,10 +5,16 @@ import java.io.FileInputStream;
 public class EvenNumberFile {
     public static void main(String[] args) {
         try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
-                if (read % 2 == 0) {
-                    System.out.println(read);
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (int i = 0; i < lines.length; i++) {
+                int res = Integer.parseInt(lines[i]);
+                if (res % 2 == 0) {
+                    System.out.println(res);
                 }
             }
         } catch (Exception e) {
