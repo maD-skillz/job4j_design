3c19d659db15bc97eeb0b9718fe1db70065f30fe
maD-skillz <warneverchang3s@gmail.com>
2021-09-25 12:22:02 +0300

    0.3. BufferedReader. [#252489 #207307]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,28 +2,19 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     public static List<String> filter(String file) {
-        String[] searchNumber;
-        String search = "404";
-        String toFilter;
-        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            StringBuilder text = new StringBuilder();
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String lineSplit : lines) {
-                searchNumber = lineSplit.split("\\s");
-                if (searchNumber[searchNumber.length - 1].equals(search)) {
-                    toFilter = searchNumber[searchNumber.length - 1];
-                    return in.lines().filter(e -> e.contains(toFilter)).collect(Collectors.toList());
-                }
-            }
+        List<String> list = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
+            return in.lines().filter(s -> s.contains("404")).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+                return list;
     }
 
     public static void main(String[] args) {
