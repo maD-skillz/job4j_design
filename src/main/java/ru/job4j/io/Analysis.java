08457e73fca102834f065aa92ebd73692fa8acda
maD-skillz <warneverchang3s@gmail.com>
2021-10-04 10:51:18 +0300

    2. Анализ доступности сервера. [#859 #211017]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,55 +1,36 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class Analysis {
 
     public void unavailable(String source, String target) {
-        List<String> result = new ArrayList<>();
-        StringJoiner temp = new StringJoiner(";");
-        Iterator<String> it = reader(source).iterator();
-        reader(source).forEach(e -> {
-            while (it.hasNext()) {
-                String[] data = it.next().split(" ");
-                if (data[0].equals("400") || data[0].equals("500")) {
-                    temp.add(data[1]);
-                    result.add(temp.toString());
-                    break;
-                } else if (data[0].equals("200") || data[0].equals("300")) {
-                    temp.add(data[1]);
-                    result.add(temp.toString());
-                    break;
-                }
-            }
-        });
-        writer(target, result);
-    }
-
-    private List<String> reader(String source) {
-        List<String> list = new ArrayList<>();
-        try (BufferedReader out = new BufferedReader(
-                new FileReader(source))) {
-          list = out.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    public void writer(String target, List<String> list) {
-        try (PrintWriter in = new PrintWriter(
-                new FileOutputStream(target))) {
-            list.stream().forEach(e -> in.write(e + System.lineSeparator()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static void main(String[] args) {
-        Analysis analysis = new Analysis();
-        analysis.unavailable("server_log.csv", "server_offline.csv");
+      try (BufferedReader in = new BufferedReader(new FileReader(source));
+      BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
+          StringBuilder builder = new StringBuilder();
+          in.lines().forEach(e -> {
+              String[] data = e.split(" ");
+              if (data[0].equals("400") || data[0].equals("500")) {
+                 if (builder.length() == 0) {
+                     builder.append(data[1]);
+                 }
+              } else if (data[0].equals("200") || data[0].equals("300")) {
+                  if (builder.length() != 0) {
+                      builder.append(";").append(data[1]);
+                      try {
+                          out.write(builder.toString() + System.lineSeparator());
+                      } catch (IOException ioException) {
+                          ioException.printStackTrace();
+                      }
+                      builder.setLength(0);
+                  }
+              }
+          });
+          if (builder.length() != 0) {
+              out.write(builder.toString());
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
     }
 }
---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class AnalysisTest {
+    private final String source = "server_log.csv";
+    private final String target = "server_offline.csv";
+
+    @Test
+    public void serverCheck1() {
+        Analysis analysis = new Analysis();
+        writeToFile(source, List.of(
+                "200 10:56:01",
+        "500 10:57:01",
+        "400 10:58:01",
+        "200 10:59:01",
+        "500 11:01:02",
+        "200 11:02:02"
+        ));
+        analysis.unavailable(source, target);
+        assertThat(getFileInfo(target),
+                is("[10:57:01;10:59:01, "
+                        + "11:01:02;11:02:02]"));
+    }
+
+    @Test
+    public void serverCheck2() {
+        Analysis analysis = new Analysis();
+        writeToFile(source, List.of(
+                "200 10:56:01",
+        "500 10:57:01",
+        "400 10:58:01",
+        "500 10:59:01",
+        "400 11:01:02",
+        "200 11:02:02"
+        ));
+        analysis.unavailable(source, target);
+        assertThat(getFileInfo(target), is("[10:57:01;11:02:02]"));
+    }
+
+    private String getFileInfo(String path) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(path)
+        )) {
+            list = reader.lines().collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list.toString();
+    }
+
+    private void writeToFile(String file, List<String> list) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(file))) {
+            list.forEach(e -> out.write(e + System.lineSeparator()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
