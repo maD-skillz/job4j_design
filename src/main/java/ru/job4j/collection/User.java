61afe35f5271d254701e8e21fd90d9d7cce257bc
maD-skillz <warneverchang3s@gmail.com>
2021-10-07 12:56:53 +0300

    Правки проекта

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -16,8 +16,6 @@ public class User {
         this.birthday = birthday;
     }
 
-
-
     public static void main(String[] args) {
         Calendar cal = new GregorianCalendar(1900, Calendar.DECEMBER, 31);
         User user1 = new User("Ash", 2, cal);
@@ -25,11 +23,11 @@ public class User {
         Map<User, Object> userMap = new HashMap<>();
         userMap.put(user1, new Object());
         userMap.put(user2, new Object());
-    //    if (!userMap.get(user1).equals(userMap.get(user2))) {
-    //        System.out.println("false");
-    //    }
-    //    System.out.println(userMap.get(user1));
-    //    System.out.println(userMap.get(user2));
+        if (!userMap.get(user1).equals(userMap.get(user2))) {
+            System.out.println("false");
+        }
+        System.out.println(userMap.get(user1));
+        System.out.println(userMap.get(user2));
         int x = 8;
         int y = 8 % 3;
         System.out.println(y);
