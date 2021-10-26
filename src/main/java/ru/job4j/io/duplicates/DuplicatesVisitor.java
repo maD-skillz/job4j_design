c8914b73deadc2eaa1acab4a979e0e265956acc0
maD-skillz <warneverchang3s@gmail.com>
2021-10-26 13:58:57 +0300

    4.2. Поиск дубликатов [#315066 #212086]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -2,9 +2,11 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -12,22 +14,25 @@ import java.util.List;
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     private FileProperty nextFile;
-    private HashMap<FileProperty, List<Path>> map;
-    public List<Path> duplicates;
+    private HashMap<FileProperty, List<Path>> map = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        long fileSize = Files.size(file);
+        String fileName = file.getFileName().toString();
+        nextFile = new FileProperty(fileSize, fileName);
         if (map.containsKey(nextFile)) {
-            duplicates = map.get(nextFile);
+            map.get(nextFile).add(file);
         } else {
+            List<Path> duplicates = new ArrayList<>();
             duplicates.add(file.toAbsolutePath());
             map.put(nextFile, duplicates);
         }
-        return super.visitFile(file, attrs);
+        return FileVisitResult.CONTINUE;
     }
 
     public List<Path> getDuplicates() {
+        List<Path> duplicates = new ArrayList<>();
         for (Path key : map.get(nextFile)) {
             if (map.get(nextFile).size() > 1) {
                 duplicates.add(key);
