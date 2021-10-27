8bde295c074aa339fd69bb435329f99a87bc920f
maD-skillz <warneverchang3s@gmail.com>
2021-10-27 17:44:58 +0300

    0. Что такое база данных. [#6860 #220765]

--- sql_example1.sql
+++ sql_example1.sql
@@ -5,3 +5,12 @@ create table example_cars1(
 	rangeOf int,
 	isTruck boolean
 );
+
+insert into example_cars1(name, nameOf, rangeOf, isTruck) 
+values('Chevy', 'Tahoe', 400, false);
+
+select * from example_cars1;
+
+update example_cars1 set nameOf = 'Suburban';
+
+delete from example_cars1;
