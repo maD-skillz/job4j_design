7ba807ca0d892db46a9c71280d9c5ce2f31f550b
maD-skillz <warneverchang3s@gmail.com>
2021-10-29 17:31:05 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,20 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar search.jar ROOT_FOLDER.");
+        }
+
+        if (!args[0].startsWith(".")) {
+            throw new IllegalArgumentException("Path must stars with '.'.");
+        }
+
+        if (!args[1].endsWith("txt")) {
+            throw new IllegalArgumentException("Path end must be 'js'.");
+        }
         Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith("txt")).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
