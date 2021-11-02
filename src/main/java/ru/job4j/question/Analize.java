aeca6a9172c4baeeb6be9d8208a86474bf2b12fd
maD-skillz <warneverchang3s@gmail.com>
2021-11-02 23:00:17 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,5 +1,7 @@
 package ru.job4j.question;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Analize {
@@ -10,6 +12,8 @@ public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info inf = new Info(0, 0, 0);
+        Map<String, Integer> mapPrev = new HashMap<>();
+        Map<String, Integer> mapCur = new HashMap<>();
         for (User prevUser : previous) {
             if (!current.contains(prevUser)) {
                 inf.setDeleted(countDeleted++);
@@ -22,14 +26,33 @@ public class Analize {
             }
         }
 
-        for (User userGetPrev : previous) {
-            for (User userGetCur : current) {
-                if (userGetPrev.getId() == userGetCur.getId()
-                        && !userGetPrev.getName().equals(userGetCur.getName())) {
+        for (User i : previous) {
+            mapPrev.put(i.getName(), i.getId());
+        }
+
+        for (User j : current) {
+            mapCur.put(j.getName(), j.getId());
+        }
+
+        for (Map.Entry<String, Integer> ind : mapPrev.entrySet()) {
+            for (Map.Entry<String, Integer> ind2 : mapCur.entrySet()) {
+                if (!ind.getKey().equals(ind2.getKey())
+                && ind.getValue().equals(ind2.getValue())) {
                     inf.setChanged(countChanged++);
                 }
             }
         }
+
         return inf;
     }
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
 }
