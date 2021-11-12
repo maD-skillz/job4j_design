eb075da5cfaea4b8bf9554df374a03dcf1a80811
maD-skillz <warneverchang3s@gmail.com>
2021-11-12 18:11:17 +0300

    2. Группировка и агрегатные функции [#373618]

---
+++ dev_prices.sql
@@ -0,0 +1,56 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    devices_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into devices(name, price) values('Watch', '4700');
+insert into devices(name, price) values('Phone', '18000');
+insert into devices(name, price) values('Notebook', '70000');
+
+insert into people(name) values('Иван');
+insert into people(name) values('Игнат');
+insert into people(name) values('Сифон');
+
+insert into devices_people(devices_id, people_id) values(1, 1);
+insert into devices_people(devices_id, people_id) values(1, 2);
+insert into devices_people(devices_id, people_id) values(1, 3);
+insert into devices_people(devices_id, people_id) values(2, 1);
+insert into devices_people(devices_id, people_id) values(2, 3);
+insert into devices_people(devices_id, people_id) values(3, 2);
+insert into devices_people(devices_id, people_id) values(3, 3);
+
+select avg(price) from devices;
+
+select
+ppl.name,
+avg(dprice.price)
+from devices as dprice,
+people as ppl
+join devices_people dppl
+on dppl.people_id = ppl.id
+group by ppl.name;
+
+select
+ppl.name,
+avg(dprice.price)
+from devices as dprice,
+people as ppl
+join devices_people dppl
+on dppl.people_id = ppl.id
+group by ppl.name
+having avg(dprice.price) > '5000';
+
+
+
