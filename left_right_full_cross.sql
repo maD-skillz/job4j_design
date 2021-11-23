cea65c6a8277abdb88a7bb09d0c9de77ce5609b3
maD-skillz <warneverchang3s@gmail.com>
2021-11-23 18:03:08 +0300

    0. Join [#6862 #227881]

--- left_right_full_cross.sql
+++ left_right_full_cross.sql
@@ -20,13 +20,23 @@ insert into employe(name, dep_id) values('Stan', 1);
 insert into employe(name, dep_id) values('Sam', 3);
 insert into employe(name, dep_id) values('John', 2);
 
-select * from
-departments d 
-left join employe e 
-on e.dep_id = d.id;
+select
+d.id dep_id,
+d.name dep_n,
+e.id emp_id,
+e.name emp_name,
+e.dep_id depart_id
+from employe e
+left join departments d
+on d.id = e.dep_id;
 
-select * from
-employe e
+select
+d.id dep_id,
+d.name dep_n,
+e.id emp_id,
+e.name emp_name,
+e.dep_id depart_id
+from employe e
 right join departments d
 on e.dep_id = d.id;
 
@@ -44,6 +54,3 @@ on e.dep_id = d.id;
 select * from
 employe e
 cross join departments d;
-
-
-
