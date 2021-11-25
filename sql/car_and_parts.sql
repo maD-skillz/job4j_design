ca0a93f8845990ec512b740c550c773a65ac5ba3
maD-skillz <warneverchang3s@gmail.com>
2021-11-25 16:40:30 +0300

    Хранилище машин [#1733 #232447]

--- sql/car_and_parts.sql
+++ sql/car_and_parts.sql
@@ -1,51 +1,47 @@
 create table car(
 	id serial primary key,
-	name varchar(255),
-	body int,
-	engine int,
-	transmission int
+	name varchar(255)
 );
 
 create table body(
 	id serial primary key,
 	name varchar(255),
-	body_id int references body(id)
+	body_id int references car(id)
 );
 
 create table engine(
 	id serial primary key,
 	name varchar(255),
-	engine_id int references engine(id)
+	engine_id int references car(id)
 );
 
 	create table transmission(
 	id serial primary key,
 	name varchar(255),
-	trans_id int references transmission(id)
+	trans_id int references car(id)
 );
 
+insert into car(name) values('Tahoe');
+insert into car(name) values('F-150');
+insert into car(name) values('Camry');
+insert into car(name) values('Express');
 
-insert into body(name, body_id) values('SUV', '1');
-insert into body(name, body_id) values('Truck', '2');
-insert into body(name, body_id) values('Sedan', '3');
-insert into body(name, body_id) values('Minivan', '4');
+insert into body(name, body_id) values('SUV', '13');
+insert into body(name, body_id) values('Truck', '14');
+insert into body(name, body_id) values('Sedan', '15');
+insert into body(name, body_id) values('Minivan', '16');
 
-insert into engine(name, engine_id) values('2.0L R4', '5');
-insert into engine(name, engine_id) values('3.5L V6', '6');
-insert into engine(name, engine_id) values('6.0L V8', '7');
-insert into engine(name, engine_id) values('3.5L V6 turbo', '8');
-insert into engine(name, engine_id) values('2.0L R4 turbo', '9');
+insert into engine(name) values('2.0L R4');
+insert into engine(name, engine_id) values('3.5L V6', '15');
+insert into engine(name, engine_id) values('6.0L V8', '13');
+insert into engine(name, engine_id) values('3.5L V6 turbo', '14');
+insert into engine(name, engine_id) values('2.0L R4 turbo', '16');
 
-insert into transmission(name, trans_id) values('5 speed auto', '10');
-insert into transmission(name, trans_id) values('6 speed auto', '11');
-insert into transmission(name, trans_id) values('6 speed robotic', '12');
-insert into transmission(name, trans_id) values('5 speed manual', '13');
-insert into transmission(name, trans_id) values('6 speed manual', '14');
-
-insert into car(name, body, engine, transmission) values('Tahoe', '1', '7', '11');
-insert into car(name, body, engine, transmission) values('F-150', '2', '8', '10');
-insert into car(name, body, engine, transmission) values('Camry', '3', '6', '12');
-insert into car(name, body, engine, transmission) values('Express', '4', '9', '14');
+insert into transmission(name, trans_id) values('5 speed auto', '14');
+insert into transmission(name, trans_id) values('6 speed auto', '13');
+insert into transmission(name, trans_id) values('6 speed robotic', '15');
+insert into transmission(name) values('5 speed manual');
+insert into transmission(name, trans_id) values('6 speed manual', '16'););
 
 
 select
@@ -55,23 +51,28 @@ e.name as Engine,
 t.name as Trans
 from car c
 join body b
-on c.body = b.id
+on b.body_id = c.id
 join engine e
-on c.engine = e.id
+on e.engine_id = c.id
 join transmission t
-on c.transmission = t.id;
+on t.trans_id = c.id;
 
 select b
 from body b
 where not exists(select * from car c
-where c.body = b.id);
+where b.body_id = c.id);
 
 select e
 from engine e
 where not exists(select * from car c
-where c.engine = e.id);
+where e.engine_id = c.id);
 
 select t
 from transmission t
 where not exists(select * from car c
 where c.transmission = t.id);
+
+select t
+from transmission t
+where not exists(select * from car c
+where t.trans_id = c.id);
