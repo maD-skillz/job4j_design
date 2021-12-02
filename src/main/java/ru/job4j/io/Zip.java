bfdfd71a13ab5f3e8b0163f936c4c8cee3d1614f
maD-skillz <warneverchang3s@gmail.com>
2021-12-02 17:26:59 +0300

    5.2. Архивировать проект [#861 #234008]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,89 +3,50 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public static final Map<String, String> arguments = new HashMap<>();
+    public static void packFiles(List<Path> sources, Path target) {
+        try (ZipOutputStream zipFiles = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
+            for (Path i : sources) {
+                zipFiles.putNextEntry(new ZipEntry(i.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(i.toFile()))) {
+                    zipFiles.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-    public static List<Path> searcher;
+        public static void main(String[] args) throws IOException {
+            String getDir;
+            String skipFile;
+            String targetDir;
 
-    public static void parse(String[] args) {
         if (args.length == 0) {
             throw new IllegalArgumentException("Аргументы отсутствуют. Введите аргументы.");
         }
 
-        for (String i : args) {
-            String[] getKeyAndVal = i.split("=");
-            if (getKeyAndVal.length == 2) {
-                if (getKeyAndVal[0].startsWith("-")) {
-                    arguments.put(getKeyAndVal[0].substring(1), getKeyAndVal[1]);
-                }
-                if (getKeyAndVal[1].startsWith("-")) {
-                    arguments.put(getKeyAndVal[1].substring(1), getKeyAndVal[0]);
-                }
-                if (!getKeyAndVal[0].startsWith("-")) {
-                    throw new IllegalArgumentException("Аргументы должны начинаться со знака '-'");
-                }
-            } else {
-                throw new IllegalArgumentException("Неверное количество аргументов.");
-            }
+        if (args.length == 3) {
+            ArgsName argsName = ArgsName.of(args);
+           getDir = argsName.get("d");
+           skipFile = argsName.get("e");
+           targetDir = argsName.get("o");
+        } else {
+            throw new IllegalArgumentException("Неверное количество аргументов.");
         }
-    }
 
-    public static void packFiles(List<Path> sources, Path target) {
-        for (Path i : sources) {
-            searcher = search(i, p -> p.endsWith(arguments.get("-d"))
-                    && !p.toFile().getName().endsWith(arguments.get("-e")));
-        }
-        try (ZipOutputStream zipFiles = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
-            zipFiles.putNextEntry(new ZipEntry((ZipEntry) searcher));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream((File) searcher))) {
-                    zipFiles.write(out.readAllBytes());
-                }
-            } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        Predicate<Path> condition = p -> !p.toFile().getName().endsWith(skipFile);
 
-    public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        List<Path> searchFile = Search.search(Paths.get(getDir), condition);
 
-    public static List<Path> search(Path root, Predicate<Path> condition) {
-        SearchFiles searcher = new SearchFiles(condition);
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(root.toFile().getAbsolutePath()))) {
-            out.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return searcher.getPaths();
-    }
-
-    public static void main(String[] args) throws IOException {
-        parse(args);
-
-        packFiles(searcher,
-               Paths.get(arguments.get(args[2]))
-        );
-
-
-        packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
+        packFiles(searchFile,
+               Paths.get(targetDir)
         );
     }
 }
